347de1acc2e29fad55fa8e770f8177ef4014949e
sapiens12 <sapiens12@nate.com>
2024-05-31 00:53:55 +0200

    backend 설정

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const indexRouter = require("./routes/index");
 
 require("dotenv").config();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const mongoURI = process.env.OCAL_DB_ADDRESS;
+app.use("/api", indexRouter);
+
+const mongoURI = process.env.LOCAL_DB_ADDRESS;
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(() => console.log("mongoose connected"))
